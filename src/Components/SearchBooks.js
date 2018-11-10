e65c721e1d22bfc81746eaa2959254100e91e6f3
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2018-11-10 20:28:11 +0000

    Step 1 - Create components that hold data.

---
+++ src/Components/SearchBooks.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import * as BooksAPI from '../BooksAPI'
+
+// Step 1 - Create components that hold data.
+
+class SearchBooks extends Component {
+  state = {
+    query: '',
+    booksFound: []
+  }
+
+  // Created Lifecycle Event to Search for books in the API
+  componentDidMount() {
+    this.performSearch()
+  }
+
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+    this.performSearch(query)
+  }
+
+  performSearch = (query) => {
+    BooksAPI.search(query)
+      .then((booksFound) => {
+        this.setState(() => ({
+          booksFound,
+        }))
+      })
+  }
+
+  render() {
+
+    console.log(this.state.booksFound)
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <a
+            className="close-search"
+            onClick={this.props.toggleScreen}
+          >Close
+          </a>
+          <div className="search-books-input-wrapper">
+            {/*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+
+            />
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">{JSON.stringify(this.state)}</ol>
+        </div>
+      </div>
+    )
+
+  }
+
+}
+
+export default SearchBooks
